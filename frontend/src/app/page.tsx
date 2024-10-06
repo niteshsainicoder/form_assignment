978c4e3143521cd311784d16f33e8de7a3f08866
niteshsainicoder <kumarsundratijara@gmail.com>
2024-10-06 13:05:02 +0530

    frontend Ready

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,101 +1,36 @@
-import Image from "next/image";
+'use client';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import FormComponent from '../components/FormComponent';
+import axios from 'axios';
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+function App() {
+    const refresh= async()=>{
+    try{
+        const response = await axios.get('http://localhost:3001/api/form/sync');
+        if (response.status === 200) {
+            console.log('Data successfully synced to Google Sheet!');
+            alert('Data successfully synced to Google Sheet!');
+        } else {
+            console.log('Failed to sync data to Google Sheet.');
+        }
+    }
+    catch(error){
+        console.error('Error syncing data to SheetDB:', error);
+    }
+    }
+    return (
+        <Router>
+            <div className='w-full min-h-screen max-h-fit pt-10 flex flex-col relative bg-neutral-800 justify-center items-center gap-8'>
+                <h1 className='w-fit px-20 py-2  rounded-lg bg-zinc-700 text-center'>Dynamic Forms</h1>
+                <div className='absolute top-5 w-fit h-fit right-5'><button type='button' onClick={refresh} className='bg-green-700 p-2 px-6 rounded-lg'>Refresh </button></div>
+              <div className='flex gap-5 items-center'>  <Link to="/form-a" className='bg-cyan-700 p-2 px-6 rounded-lg'>Form A</Link> || <Link to="/form-b" className='bg-cyan-700 p-2 px-6 rounded-lg'>Form B</Link></div>
+               <Routes> <Route path="/form-a" element={<FormComponent FormType="Form A" />} />
+                <Route path="/form-b" element={<FormComponent FormType="Form B" />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
+
+export default App;
---
+++ frontend/src/components/FormComponent.tsx
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+interface err {
+    name: string,
+    countryCode: string,
+    phoneNumber: string
+}
+function FormComponent({ FormType }: {
+    FormType: string
+}) {
+    const [name, setName] = useState('');
+    const [countryCode, setCountryCode] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const [errors, setErrors] = useState<err>({ name: '', countryCode: '', phoneNumber: '' });
+
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        const newErrors: err = { name: '', countryCode: '', phoneNumber: '' };
+
+        // Simple validation
+        if (!/^[A-Za-z]+$/.test(name)){ newErrors.name = 'only letters'; return ; }
+        if (!countryCode) {newErrors.countryCode = ' country code'; return ; }
+        if (!/^\d+$/.test(phoneNumber)) { newErrors.phoneNumber = 'only numeric'; return ; }
+
+
+        // Save to SQL
+        console.log('clicked');
+        const formType = FormType == "Form A" ? 'formA' : 'formB';
+        await axios.post('http://localhost:3001/api/form/submit', { formType, name, countryCode, phoneNumber }).then(() => {
+            alert('Form data saved successfully!');
+        }).catch((error) => {
+            console.error('Error saving form data:', error);
+        });
+        localStorage.setItem('formData', JSON.stringify({ name, countryCode, phoneNumber }));
+
+
+        setErrors(newErrors);
+    };
+
+    useEffect(() => {
+        const storedData = localStorage.getItem('formData');
+        if (storedData) {
+            const { name, countryCode, phoneNumber } = JSON.parse(storedData);
+            setName(name);
+            setCountryCode(countryCode);
+            setPhoneNumber(phoneNumber);
+        }
+    }, []);
+
+    return (
+        <form onSubmit={handleSubmit} className='flex w-[290px] sm:w-[400px] py-7 flex-col gap-5 border border-gray-500 rounded-lg p-5 justify-evenly items-center'>
+            <h2>{FormType}</h2>
+            <div className='w-full flex flex-wrap sm:flex-nowrap relative'>
+                <label className='w-3/6 '>Name:</label>
+                <input type="text" placeholder='Enter Name' value={name} onChange={(e) => setName(e.target.value)} className='bg-neutral-600 p-1 px-6 rounded-lg outline-none' />
+                {errors.name && <p className='absolute -top-5 left-1/3 text-red-500 font-bold antialiased'>{errors.name}</p>}
+            </div>
+            <div className='w-full flex flex-wrap sm:flex-nowrap relative'>
+                <label className='max-w-3/6 w-3/6'>Country Code:</label>
+                <select aria-label='countryCode' value={countryCode} onChange={(e) => setCountryCode(e.target.value)} className='bg-neutral-600 p-1 px-6 rounded-lg outline-none' >
+                    <option value="">Select Code</option>
+                    <option value="+91">+91</option>
+                    <option value="+1">+1</option>
+                </select>
+                {errors.countryCode && <p className='absolute -top-5 left-1/3 text-red-500 font-bold antialiased'>{errors.countryCode}</p>}
+            </div>
+            <div className='w-full flex flex-wrap   sm:items-start sm:flex-nowrap  relative'>
+                <label className='w-3/6 '>Phone Number:</label>
+                <input type="text" placeholder='Enter Phone Number' value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} className='bg-neutral-600 p-1 px-6 rounded-lg outline-none' />
+                {errors.phoneNumber && <p className='absolute -top-5 left-1/3 text-red-500 font-bold antialiased'>{errors.phoneNumber}</p>}
+            </div>
+            <button type="submit" className='bg-red-600 hover:bg-red-700 p-2 px-6 rounded-lg'>Submit</button>
+        </form>
+    );
+}
+
+export default FormComponent;
