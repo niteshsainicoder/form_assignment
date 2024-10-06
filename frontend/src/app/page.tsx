267721d71ee800b9c7ecb3ec097b47cdfa753a8a
niteshsainicoder <kumarsundratijara@gmail.com>
2024-10-06 17:49:54 +0530

    updated

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,35 +1,42 @@
 'use client';
-import React from 'react';
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
-import FormComponent from '../components/FormComponent';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import FormComponent from '@/components/FormComponent';
 
 function App() {
-    const refresh= async()=>{
-    try{
-        const response = await axios.get('http://localhost:3001/api/form/sync');
-        if (response.status === 200) {
-            console.log('Data successfully synced to Google Sheet!');
-            alert('Data successfully synced to Google Sheet!');
-        } else {
-            console.log('Failed to sync data to Google Sheet.');
+    const [formbutton, setformbutton] = useState('');
+    const refresh = async () => {
+        try {
+            const response = await axios.get('http://localhost:3001/api/form/sync');
+            if (response.status === 200) {
+                console.log('Data successfully synced to Google Sheet!');
+                alert('Data successfully synced to Google Sheet!');
+            } else {
+                console.log('Failed to sync data to Google Sheet.');
+            }
+        } catch (error) {
+            console.error('Error syncing data to SheetDB:', error);
         }
-    }
-    catch(error){
-        console.error('Error syncing data to SheetDB:', error);
-    }
-    }
+    };
+    useEffect(() => {
+        setformbutton('');
+    }, []);
+
     return (
-        <Router>
-            <div className='w-full min-h-screen max-h-fit pt-10 flex flex-col relative bg-neutral-800 justify-center items-center gap-8'>
-                <h1 className='w-fit px-20 py-2  rounded-lg bg-zinc-700 text-center'>Dynamic Forms</h1>
-                <div className='absolute top-5 w-fit h-fit right-5'><button type='button' onClick={refresh} className='bg-green-700 p-2 px-6 rounded-lg'>Refresh </button></div>
-              <div className='flex gap-5 items-center'>  <Link to="/form-a" className='bg-cyan-700 p-2 px-6 rounded-lg'>Form A</Link> || <Link to="/form-b" className='bg-cyan-700 p-2 px-6 rounded-lg'>Form B</Link></div>
-               <Routes> <Route path="/form-a" element={<FormComponent FormType="Form A" />} />
-                <Route path="/form-b" element={<FormComponent FormType="Form B" />} />
-                </Routes>
+        <div className='w-full min-h-screen max-h-fit pt-10 flex flex-col relative bg-neutral-800 justify-center items-center gap-8'>
+            <h1 className='w-fit px-20 py-2 rounded-lg bg-zinc-700 text-center'>Dynamic Forms</h1>
+            <div className='absolute top-5 w-fit h-fit right-5'>
+                <button type='button' onClick={refresh} className='bg-green-700 p-2 px-6 rounded-lg'>Refresh</button>
             </div>
-        </Router>
+            {
+                formbutton == '' ? <div className='flex gap-5 items-center'>
+                    <button type={`button`} onClick={() => setformbutton('Form A')} className='bg-cyan-700 p-2 px-6 rounded-lg'>Form A</button>
+                    ||
+                    <button type={`button`} onClick={() => setformbutton('Form B')} className='bg-cyan-700 p-2 px-6 rounded-lg'>Form B</button>
+                </div> : <FormComponent FormType={formbutton} />
+
+            }
+        </div>
     );
 }
 
--- frontend/src/components/FormComponent.tsx
+++ frontend/src/components/FormComponent.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useRouter } from 'next/navigation';
 interface err {
     name: string,
     countryCode: string,
@@ -8,6 +9,7 @@ interface err {
 function FormComponent({ FormType }: {
     FormType: string
 }) {
+    const router =useRouter();
     const [name, setName] = useState('');
     const [countryCode, setCountryCode] = useState('');
     const [phoneNumber, setPhoneNumber] = useState('');
@@ -29,6 +31,7 @@ function FormComponent({ FormType }: {
         const formType = FormType == "Form A" ? 'formA' : 'formB';
         await axios.post('http://localhost:3001/api/form/submit', { formType, name, countryCode, phoneNumber }).then(() => {
             alert('Form data saved successfully!');
+            router.push('/')
         }).catch((error) => {
             console.error('Error saving form data:', error);
         });
@@ -49,16 +52,16 @@ function FormComponent({ FormType }: {
     }, []);
 
     return (
-        <form onSubmit={handleSubmit} className='flex w-[290px] sm:w-[400px] py-7 flex-col gap-5 border border-gray-500 rounded-lg p-5 justify-evenly items-center'>
+        <form onSubmit={handleSubmit} className='flex w-[290px] sm:w-[400px] h-fit py-7 flex-col gap-5 border border-gray-500 rounded-lg p-5 justify-evenly items-center'>
             <h2>{FormType}</h2>
             <div className='w-full flex flex-wrap sm:flex-nowrap relative'>
-                <label className='w-3/6 '>Name:</label>
+                <label className='w-1/2 max-w-1/2 '>Name:</label>
                 <input type="text" placeholder='Enter Name' value={name} onChange={(e) => setName(e.target.value)} className='bg-neutral-600 p-1 px-6 rounded-lg outline-none' />
                 {errors.name && <p className='absolute -top-5 left-1/3 text-red-500 font-bold antialiased'>{errors.name}</p>}
             </div>
-            <div className='w-full flex flex-wrap sm:flex-nowrap relative'>
-                <label className='max-w-3/6 w-3/6'>Country Code:</label>
-                <select aria-label='countryCode' value={countryCode} onChange={(e) => setCountryCode(e.target.value)} className='bg-neutral-600 p-1 px-6 rounded-lg outline-none' >
+            <div className='w-full flex   flex-wrap sm:flex-nowrap sm:gap-4 justify-between relative'>
+                <label className='max-w-1/3   sm:w-1/3'>Country Code:</label>
+                <select aria-label='countryCode' value={countryCode} onChange={(e) => setCountryCode(e.target.value)} className='bg-neutral-600 w-fit p-1  rounded-lg outline-none' >
                     <option value="">Select Code</option>
                     <option value="+91">+91</option>
                     <option value="+1">+1</option>
